f775f896d1079677321441f686b2d7e61b7a85bd
david-intersys <drodriguez@intersysconsulting.com>
2023-11-13 17:24:58 -0600

    View added for jokes

---
+++ src/main/java/guru/springframework/springjokesappv2/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.springjokesappv2.controllers;
+
+import guru.springframework.springjokesappv2.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "index";
+    }
+}
